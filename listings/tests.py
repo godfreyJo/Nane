47c7361e6f6f415e79323be4adbc5efc67a09358
godfreyJo <godfreyoyugi@gmail.com>
2020-05-29 12:14:36 +0300

    cleaning the app

--- listings/tests.py
+++ listings/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Listing
 
-# Create your tests here.
+class ModelsTestCase(TestCase):
+
+    def setUp(self):
+
+
+         
+        return super().setUp()
+
